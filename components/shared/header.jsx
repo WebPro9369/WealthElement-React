40174fcde3542acd8d4c817970ac987e93bdcf1c
Marcel Song <songhc666@gmail.com>
2018-11-23 16:51:29 +0800

    Created shared components: header and footer

---
+++ components/shared/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from 'antd'
+
+const Header = () => (
+  <div className="header-wrapper">
+    <div className="header-logo-wrapper">
+      **LOGO**
+    </div>
+    <div className="header-links-wrapper">
+      <Link href="/"><a className="header-link">Home</a></Link>
+      <Link href="#"><a className="header-link">Insights</a></Link>
+      <Link href="#"><a className="header-link">Log in</a></Link>
+      <Button className="header-button-try">Try It Now</Button>
+    </div>
+  </div>
+)
+
+export default Header;
