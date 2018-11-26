47aa2f222781e54c0a7740f794307cc9e4340ec3
Marcel Song <songhc666@gmail.com>
2018-11-26 11:12:56 +0800

    Header: used PopMenu in responsive mode

--- components/shared/header.jsx
+++ components/shared/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
-import { Button } from 'antd'
+import { Button, List, Popover } from 'antd'
+import PopMenu from './PopMenu'
 
 const Header = () => (
   <div className="header-wrapper">
@@ -12,6 +13,23 @@ const Header = () => (
       <Link href="/insights"><a className="header-link">Insights</a></Link>
       <Link href="#"><a className="header-link">Log in</a></Link>
       <Button className="header-button-try we-button">Try It Now</Button>
+    </div>
+    <div className="header-popover-wrapper">
+      <PopMenu
+        dataSource={[{
+          text: 'Home',
+          link: '/'
+         }, {
+           text: 'Insights',
+           link: '/insights'
+         }, {
+           text: 'Log In',
+           link: '#'
+        }, {
+          text: 'Try it Now',
+          link: '#'
+        }]}
+      />
     </div>
   </div>
 )
