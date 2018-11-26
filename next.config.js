6f88c7c59777e4625e9f696764e0f6fd1f503b4e
Marcel Song <songhc666@gmail.com>
2018-11-27 01:15:04 +0800

    Updated Next configurations

--- next.config.js
+++ next.config.js
@@ -1,25 +1,35 @@
-const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass')
-const withImages = require('next-images')
+const {
+  PHASE_PRODUCTION_SERVER
+} = process.env.NODE_ENV === 'development' ? {} : require('next-server/constants');
 
-module.exports = withImages(withCSS(withSass({
-  cssModules: false,
-  webpack: (config, {
-    isServer
-  }) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(jpg|png|gif|svg|pdf|ico)$/,
-        use: [{
-          loader: 'file-loader',
-        }]
-      })
-    }
-
-    return config
+module.exports = (phase, {
+  defaultConfig
+}) => {
+  console.log("In Next.config")
+  console.log("phase = ", phase)
+  console.log("PHASE_PRODUCTION_SERVER = ", PHASE_PRODUCTION_SERVER)
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {
+      publicRuntimeConfig: {
+        staticFolder: '/static'
+      }
+    };
   }
-})))
+
+  const withCSS = require('@zeit/next-css')
+  const withSass = require('@zeit/next-sass')
+  const withImages = require('next-images')  
+  return withImages(withCSS(withSass({
+    cssModules: false,
+    webpack: (config, {
+      isServer
+    }) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
+      return config
+    }
+  })))
+}
